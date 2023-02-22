09c8b4cb1fa16eb4ac81d5745cd641c43d48e474
Muhammad Zidane Iqbal <raja.zidaneiqbal@gmail.com>
2023-02-22 10:42:56 +0700

    [add] New File ⚙
    Upload New File

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import { createRouter, createWebHistory } from "vue-router";
+import Index from "../views/IndexView.vue";
 
-Vue.use(Router)
-
-export default new Router({
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+      path: "/",
+      name: "home",
+      component: Index,
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
+
+export default router;
---
+++ src/views/IndexView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about-me">
+    <h2>About Me</h2>
+    <img src="@/assets/prof.jpg" alt="Profile Picture" />
+    <p>Bio</p>
+    <ul>
+      <li><strong>Name:</strong> {{ name }}</li>
+      <li><strong>Location:</strong> {{ location }}</li>
+      <li><strong>Age:</strong> {{ age }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Zidane",
+      age: "20",
+      location: "Malang",
+    };
+  },
+};
+</script>
+
+<style>
+section {
+  max-width: 800px;
+  margin: auto;
+  padding: 2rem;
+}
+
+.about-me img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  margin: 1rem 0;
+}
+</style>
